4a364db6e7ec9f18f565df528ca2256367ed3177
Damyan Peykov <dppeykov@gmail.com>
2019-08-25 13:53:31 +0200

    added the router functionality + links in nav - end of 21

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this application</h1>
+      <p>This application searches the GitHub website for user accounts.</p>
+      <p>Version: 1.2 </p>
+    </div>
+  );
+};
+
+export default About;
