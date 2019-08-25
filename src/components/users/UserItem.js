bc7c8cd9edd245b0d43214ff8b0543b54256b3d7
Damyan Peykov <dppeykov@gmail.com>
2019-08-25 11:11:51 +0200

    refactoring the navbar and UserItems - end of video 13

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  state = {
-    id: "id",
-    login: "mojombo",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo"
-  };
-
-  render() {
-    const { login, avatar_url, html_url } = this.state;
-
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "80px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            See more ...
-          </a>
-        </div>
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "80px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          See more ...
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
